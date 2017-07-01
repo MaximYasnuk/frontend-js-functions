8199415cadad135b4ad340ab764a8d7a5bf17d90
MaximYasnuk <maximyasnuk@gmail.com>
2017-07-02 02:23:14 +0300

    1 test failed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 ﻿
+function min (arr){
+    if(arr===undefined){
+        return;
+    } else{
+        var minimum=arr[0];
+        var index=0;
+        for(index;index<arr.length;index+=1) {
+            if (typeof (arr[index]) == 'number') {
+                if (minimum === undefined) {
+                    minimum = arr[index];
+                    continue;
+                }
+                if (arr[index] <= minimum) {
+                    minimum = arr[index]
+                }
+            }
+        }
+    }
+    return minimum;
+}
+
+function max(arr){
+    if(arr===undefined){
+        return;
+    } else {
+        var maximum=arr[0];
+        var index=0;
+        for(index;index<arr.length;index+=1){
+            if(arr[index]>=maximum)
+                maximum=arr[index]
+        }
+        return maximum;
+    }
+}
+
+function sum(){
+    if(arguments===undefined){
+        return;
+    } else {
+        var summ=0;
+        var index=0;
+        for(index;index<arguments.length;index+=1){
+            if(typeof arguments[index] !== 'number')
+                continue;
+            summ+=arguments[index];
+        }
+        return summ;
+    }
+}
